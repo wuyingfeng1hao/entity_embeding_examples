883978caaa1cef089d65efb3c5231e7fd9b291ce
WANG Hongxing (CI/DAP41.3) <Hongxing.WANG2@cn.bosch.com>
2020-03-28 18:32:44 +0800

    updated

--- test_1.py
+++ test_1.py
@@ -16,7 +16,9 @@ print(keras.__version__)
 
 import os
 os.getcwd()
-os.chdir("C:\\Users\\WON4SZH\\Desktop\report to WangDong\\008_book_tasks\\2020_如何突破传统机器学习的瓶颈")
+
+wd = "C:\\Users\\WON4SZH\\Desktop\\report to WangDong\\008_book_tasks\\2020_如何突破传统机器学习的瓶颈\\code\\entity_examples\\entity_embeding_examples"
+os.chdir(wd)
 
 from keras.layers import Dense, Activation, Embedding,  Flatten #Merge,
 
@@ -25,9 +27,68 @@ from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 
-
-
 df = pd.read_csv('cmc.data',header=None,names=['Age','Education','H_education',
                                                      'num_child','Religion', 'Employ',
                                                      'H_occupation','living_standard',
                                                      'Media_exposure','contraceptive'])
+
+
+df.head()
+df.shape
+
+
+df.isnull().any()
+
+df.Education.hist()
+
+df.contraceptive.hist()
+
+df.dtypes
+
+def one_hot_encoding(idx):
+    y = np.zeros((len(idx),max(idx)+1))
+    y[np.arange(len(idx)), idx] = 1
+    return y
+
+scaler = StandardScaler()
+df[['Age','num_child']] = scaler.fit_transform(df[['Age','num_child']])
+
+x = df[['Age','num_child','Employ','Media_exposure']].values
+
+y = one_hot_encoding(df.contraceptive.values-1)
+
+liv_cats = df.living_standard.max()
+edu_cats = df.Education.max()
+
+liv = df.living_standard.values - 1
+liv_one_hot = one_hot_encoding(liv)
+edu = df.Education.values - 1
+edu_one_hot = one_hot_encoding(edu)
+
+train_x, test_x, train_liv, \
+test_liv, train_edu, test_edu, train_y, test_y = train_test_split(x,liv_one_hot,edu_one_hot,y,test_size=0.1, random_state=1)
+
+train_x = np.hstack([train_x, train_edu, train_liv])
+test_x = np.hstack([test_x, test_edu, test_liv])
+
+train_x.shape
+
+model = Sequential()
+
+#update to keras2 API for Dense function.
+model.add(Dense(input_dim=train_x.shape[1],units=12))
+
+model.add(Activation('relu'))
+
+model.add(Dense(units=3))
+
+model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_x, train_y, nb_epoch=100, verbose=2)
+
+model.summary()
+
+for w in model.get_weights():
+    print(w.shape)
+    
+model.evaluate(test_x, test_y, batch_size=256)
